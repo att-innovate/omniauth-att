e7771595d1086b895f7e128956d278b667172a8e
Ari Lerner <arilerner@mac.com>
2011-12-16 11:38:33 -0800

    Added docs

--- example/example_omniauth_app.rb
+++ example/example_omniauth_app.rb
@@ -52,15 +52,41 @@ class SinatraApp < Sinatra::Base
          <a href='/logout'>Logout</a>"
   end
   
+  get '/doc' do
+    erb <<-EOD
+<h2>Authentication docs page</h2>
+<p>This is a sample application that shows how the authentication mechanism works.</p>
+<p>It is incredibly simple and mimicks the OAuth2 flow. Firstly, the application must have
+a <code>client_id</code> and a <code>client_secret</code>. When the application wants to get 
+an authenticated user, they can simply redirect the user with their <code>client_id</code> and a <code>redirect_uri</code> to
+the foundry auth page at: #{auth_url}/login.
+The foundry auth will take care of the login and redirecting the user back to the <code>redirect_uri</code> (provided it matches the one that the application registered) with a <code>request_token</code>. </p>
+
+<p>It is then up to the application to respond with the <code>request_token</code> to <code>POST</code> to <code>#{auth_url}/auth</code> with the <code>request_token</code>, their <code>client_id</code> and their <code>client_secret</code>, they will get an <code>auth_hash</code> with the user's credentials, uid, some profile information and more. The entire contents of the <code>auth_hash</code> are still up for debate, but will definitely contain the user's info.</p>
+
+<p>When using the ruby language, they can use the Foundry's (soon-to-be) open-sourced <code>omniauth-att</code> library.</p>
+
+<h2>Summary</h2>
+
+<p><code>application -> 302 #{auth_url}/login</code></p>
+<p><code>#{auth_url} 302 -> application/callback?request_token=code</code></p>
+<p><code>application -> POST #{auth_url}/auth?request_token=code -> application/callback?auth_hash</code></p>
+    EOD
+  end
+  
   get '/logout' do
     session[:authenticated] = false
     redirect_to = ENV['BASE_DOMAIN'] || 'http://localhost:9393'
-    redirect (ENV['ATT_BASE_DOMAIN'] || 'https://auth.tfoundry.com') + "/logout?redirect_uri=#{CGI.escape(redirect_to)}"
+    redirect auth_url + "/logout?redirect_uri=#{CGI.escape(redirect_to)}"
   end
   
   get '/env' do
     puts ENV.inspect
     halt 401
+  end
+  
+  def auth_url
+    (ENV['ATT_BASE_DOMAIN'] || 'https://auth.tfoundry.com')
   end
   
   def base_domain
@@ -85,9 +111,15 @@ __END__
   </head>
   <body>
     <div class='container'>
+      <div class="header">
+        <ul>
+          <li><a href="/">home</a></li>
+          <li><a href="/doc">docs</a></li>
+          <li><a href="/logout">logout</a></li>
+        </ul>
+      </div>
       <div class='content'>
         <%= yield %>
-        <a href="/logout">logout</a>
       </div>
       
     </div>
